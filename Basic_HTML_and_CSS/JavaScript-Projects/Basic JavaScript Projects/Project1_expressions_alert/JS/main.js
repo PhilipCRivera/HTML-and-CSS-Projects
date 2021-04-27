772e3d1f31983c9aaa0353779087202a88bf9d60
PhilipCRivera <82003694+PhilipCRivera@users.noreply.github.com>
2021-04-26 17:22:45 -0700

    JavaScript Project updates

---
+++ Basic_HTML_and_CSS/JavaScript-Projects/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+var Family = "The Riveras", Dad = "Charlie", Mom = "Martha", //this is assigning multiple variables in one statement
+    Daughter = "Gabby", Son = "Philip", Dog = "Sparky";
+var Dad = Dad.fontcolor("blue"); // when Dad is the font is blue
+var Mom = Mom.fontcolor("red");// when Mom is written the font is red
+var Daughter = Daughter.fontcolor("yellow");// when Daughter is written the font is yellow
+var Son = Son.fontcolor("orange");// when Son is written the font is orange
+var Dog = Dog.fontcolor("purple");//when Dog is written the font is purple
+document.write(Son); // this wil display "Philip" in orange font
+
